da40db69a068fa2d46bc217a20a3a46dd4191098
AkshayRameshAppDEV <akshay_ramesh@yahoo.com>
2021-12-22 11:43:57 -0600

    Added sample quotes

--- quotes/lib/ui/quotes.dart
+++ quotes/lib/ui/quotes.dart
@@ -14,14 +14,19 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var quotesArr = ["A", "B", "C", "D", "E", "F"];
+  var quotes = [
+    "Stay Hungry. Stay Foolish - Steve Jobs",
+    "Good Artists Copy, Great Artists Steal. - Pablo Picasso",
+    "Argue with idiots, and you become an idiot. - Paul Graham",
+    "Be yourself; everyone else is already taken. - Oscar Wilde",
+    "Simplicity is the ultimate sophistication. - Leonardo Da Vinci"
+  ];
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -29,12 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   String getQuote() {
-    if(_counter < quotesArr.length) {
-      return quotesArr[_counter];
+    if (_counter < quotes.length) {
+      return quotes[_counter];
     } else {
       _counter = 0;
     }
-    return quotesArr[_counter];
+    return quotes[_counter];
   }
 
   @override
